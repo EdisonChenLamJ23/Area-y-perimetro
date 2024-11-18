55c36bac96317441d92b68641969d0cf86b7e7fa
EdisonChenLamJ23 <a20150027@juan23.edu.pe>
2024-11-17 23:24:47 -0500

    Add files via upload

---
+++ cuadrado.js
@@ -0,0 +1,13 @@
+document.write("<h1>EJercicios 03</3>");
+document.write("<h3>Area y perimetro de un cuadrado</h3>");
+var lado = parseInt(prompt("Ingrese el lado del cuadrado"));
+
+var area = lado * lado;
+var perimetro = lado * 4;
+
+document.write("El area del cuadrado es " + area);
+document.write("<br>");
+document.write("El perimetro del cuadrado es " + perimetro);
+document.write("<br>");
+document.write("<br>");
+document.write("La aplicacion fue desarrollada por: Edison Chen");
